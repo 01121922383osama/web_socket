9417fe35724436edb2e01b19c76b44b2450c9612
Osama Nabil <121755701+01121922383osama@users.noreply.github.com>
2023-09-12 22:25:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+// Middleware
+app.use(express.json());
+var clients = {};
+
+io.on("connection", (socket) => {
+    console.log("connected");
+    console.log(socket.id, "has joined");
+
+    socket.on('signin', (id) => {
+        console.log(id);
+        clients[id] = socket;
+        // console.log(clients);
+        socket.on('message', (msg) => {
+            console.log(msg);
+            let targetId = msg.targetId;
+            if (clients[targetId]) {
+                io.emit('message', msg);
+            }
+        });
+    });
+});
+app.route('/check').get((req, res) => {
+    return res.json('your app workin work');
+});
+
+server.listen(port, "0.0.0.0", () => {
+    console.log("server connected");
+});
